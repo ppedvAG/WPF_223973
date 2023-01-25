4d84a682c28d91317d774791ea1212b4d6256f1b
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-01-25 15:48:13 +0100

    UserControls

--- Trigger/MainWindow.xaml.cs
+++ Trigger/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserControls;
 
 namespace Trigger
 {
@@ -21,21 +22,38 @@ namespace Trigger
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool BoolVal { get; set; }
+
+
         public MainWindow()
         {
             InitializeComponent();
 
-            this.DataContext= this;
+            this.BoolVal = true;
+
+            //Setzen des DataContext
+            this.DataContext = this;
         }
 
-        public bool BoolVal { get; set; }
+        //EventHandler zum Ändern der Property
+        private void Btn_Ändern_Click(object sender, RoutedEventArgs e)
+        {
+            BoolVal = !BoolVal;
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BoolVal)));
+        }
+
+        private void ColorPicker_PickedColorChanged(object sender, RoutedPropertyChangedEventArgs<SolidColorBrush> e)
+        {
+            if ((sender as ColorPicker).PickedColor.ToString().Equals("#FF000000"))
+                MessageBox.Show("Alles ist schwarz");
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            BoolVal= !BoolVal;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BoolVal)));
+            MessageBox.Show(ColorPicker.GetCount(sender as Button).ToString());
         }
     }
 }
